9347c1a86e8ef9afd2832ed35a6d249dd927a12d
Red__Jack <126083814+Red05Jack@users.noreply.github.com>
2026-05-04 21:01:57 +0200

    MCP23017 Klasse
    MCP23017 Klasse neu geschrieben und verschoben.

---
+++ components/ghosttown_mcp23017/MCP23017.cpp
@@ -0,0 +1,180 @@
+#include "MCP23017.h"
+
+MCP23017::MCP23017() {
+}
+
+MCP23017::MCP23017(I2C* i2c, uint8_t address) {
+  Init(i2c, address);
+}
+
+bool MCP23017::Init(I2C* i2c, uint8_t address) {
+  if (i2c == nullptr) {
+    return false;
+  }
+
+  m_i2c = i2c;
+  m_address = address;
+
+  m_directionA = 0xFF;
+  m_directionB = 0xFF;
+  m_outputA = 0x00;
+  m_outputB = 0x00;
+
+  if (!WriteRegister(RegisterIodirA, m_directionA)) {
+    return false;
+  }
+
+  if (!WriteRegister(RegisterIodirB, m_directionB)) {
+    return false;
+  }
+
+  if (!WriteRegister(RegisterOlatA, m_outputA)) {
+    return false;
+  }
+
+  if (!WriteRegister(RegisterOlatB, m_outputB)) {
+    return false;
+  }
+
+  m_isInitialized = true;
+  return true;
+}
+
+bool MCP23017::InitPin(uint8_t pin, Mcp23017PinMode mode) {
+  if (!m_isInitialized || !IsValidPin(pin)) {
+    return false;
+  }
+
+  uint8_t* directionRegister = pin < 8 ? &m_directionA : &m_directionB;
+  const uint8_t bit = pin % 8;
+
+  if (mode == Mcp23017PinMode::Input) {
+    *directionRegister |= (1 << bit);
+  } else {
+    *directionRegister &= ~(1 << bit);
+  }
+
+  return WriteRegister(pin < 8 ? RegisterIodirA : RegisterIodirB, *directionRegister);
+}
+
+bool MCP23017::SetPin(uint8_t pin, bool state) {
+  if (!m_isInitialized || !IsValidPin(pin)) {
+    return false;
+  }
+
+  uint8_t* outputRegister = pin < 8 ? &m_outputA : &m_outputB;
+  const uint8_t bit = pin % 8;
+
+  if (state) {
+    *outputRegister |= (1 << bit);
+  } else {
+    *outputRegister &= ~(1 << bit);
+  }
+
+  return WriteRegister(pin < 8 ? RegisterOlatA : RegisterOlatB, *outputRegister);
+}
+
+bool MCP23017::SetPin(const Mcp23017Pin& pin) {
+  if (!InitPin(pin.pin, pin.mode)) {
+    return false;
+  }
+
+  if (pin.mode == Mcp23017PinMode::Output) {
+    return SetPin(pin.pin, pin.state);
+  }
+
+  return true;
+}
+
+bool MCP23017::SetAllPins(const std::array<bool, 16>& states) {
+  if (!m_isInitialized) {
+    return false;
+  }
+
+  uint8_t outputA = 0x00;
+  uint8_t outputB = 0x00;
+
+  for (uint8_t i = 0; i < 8; i++) {
+    if (states[i]) {
+      outputA |= (1 << i);
+    }
+
+    if (states[i + 8]) {
+      outputB |= (1 << i);
+    }
+  }
+
+  if (!WriteRegister(RegisterOlatA, outputA)) {
+    return false;
+  }
+
+  if (!WriteRegister(RegisterOlatB, outputB)) {
+    return false;
+  }
+
+  m_outputA = outputA;
+  m_outputB = outputB;
+
+  return true;
+}
+
+bool MCP23017::GetPin(uint8_t pin, bool& state) {
+  if (!m_isInitialized || !IsValidPin(pin)) {
+    return false;
+  }
+
+  uint8_t value = 0;
+  const uint8_t reg = pin < 8 ? RegisterGpioA : RegisterGpioB;
+
+  if (!ReadRegister(reg, value)) {
+    return false;
+  }
+
+  state = (value & (1 << (pin % 8))) != 0;
+  return true;
+}
+
+bool MCP23017::GetAllPins(std::array<bool, 16>& states) {
+  if (!m_isInitialized) {
+    return false;
+  }
+
+  uint8_t gpioA = 0;
+  uint8_t gpioB = 0;
+
+  if (!ReadRegister(RegisterGpioA, gpioA)) {
+    return false;
+  }
+
+  if (!ReadRegister(RegisterGpioB, gpioB)) {
+    return false;
+  }
+
+  for (uint8_t i = 0; i < 8; i++) {
+    states[i] = (gpioA & (1 << i)) != 0;
+    states[i + 8] = (gpioB & (1 << i)) != 0;
+  }
+
+  return true;
+}
+
+bool MCP23017::WriteRegister(uint8_t reg, uint8_t value) {
+  if (m_i2c == nullptr) {
+    return false;
+  }
+
+  const uint8_t data[2] = {reg, value};
+  return m_i2c->Write(m_address, data, sizeof(data));
+}
+
+bool MCP23017::ReadRegister(uint8_t reg, uint8_t& value) {
+  if (m_i2c == nullptr) {
+    return false;
+  }
+
+  return m_i2c->WriteRead(m_address, &reg, 1, &value, 1);
+}
+
+bool MCP23017::IsValidPin(uint8_t pin) const {
+  return pin < 16;
+}
---
+++ components/ghosttown_mcp23017/MCP23017.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+
+#include "I2C.h"
+
+enum class Mcp23017PinMode : uint8_t {
+  Output = 0,
+  Input = 1
+};
+
+struct Mcp23017Pin {
+  uint8_t pin = 0;
+  Mcp23017PinMode mode = Mcp23017PinMode::Input;
+  bool state = false;
+};
+
+class MCP23017 {
+public:
+  MCP23017();
+  MCP23017(I2C* i2c, uint8_t address = 0x20);
+
+  bool Init(I2C* i2c, uint8_t address = 0x20);
+  bool InitPin(uint8_t pin, Mcp23017PinMode mode);
+
+  bool SetPin(uint8_t pin, bool state);
+  bool SetPin(const Mcp23017Pin& pin);
+
+  bool SetAllPins(const std::array<bool, 16>& states);
+
+  bool GetPin(uint8_t pin, bool& state);
+  bool GetAllPins(std::array<bool, 16>& states);
+
+private:
+  static constexpr uint8_t RegisterIodirA = 0x00;
+  static constexpr uint8_t RegisterIodirB = 0x01;
+  static constexpr uint8_t RegisterGpioA = 0x12;
+  static constexpr uint8_t RegisterGpioB = 0x13;
+  static constexpr uint8_t RegisterOlatA = 0x14;
+  static constexpr uint8_t RegisterOlatB = 0x15;
+
+  bool WriteRegister(uint8_t reg, uint8_t value);
+  bool ReadRegister(uint8_t reg, uint8_t& value);
+
+  bool IsValidPin(uint8_t pin) const;
+
+private:
+  I2C* m_i2c = nullptr;
+  uint8_t m_address = 0x20;
+
+  uint8_t m_directionA = 0xFF;
+  uint8_t m_directionB = 0xFF;
+
+  uint8_t m_outputA = 0x00;
+  uint8_t m_outputB = 0x00;
+
+  bool m_isInitialized = false;
+};
